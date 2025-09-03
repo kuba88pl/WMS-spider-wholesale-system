679692942f8d7e0535f15a477e1efa6a49f9fc97
Jakub Kulawik <jakub.kulawik@gmail.com>
2025-09-03 14:16:56 +0200

    Add InvalidSpiderDataException and implement validation in SpiderService. Updated addSpider() method

---
+++ src/main/java/com/WMS_spiders_wholesale_system/exception/InvalidSpiderDataException.java
@@ -0,0 +1,7 @@
+package com.WMS_spiders_wholesale_system.exception;
+
+public class InvalidSpiderDataException extends RuntimeException {
+    public InvalidSpiderDataException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/WMS_spiders_wholesale_system/service/SpiderService.java
+++ src/main/java/com/WMS_spiders_wholesale_system/service/SpiderService.java
@@ -1,18 +1,23 @@
 package com.WMS_spiders_wholesale_system.service;
 
 import com.WMS_spiders_wholesale_system.entity.Spider;
+import com.WMS_spiders_wholesale_system.exception.InvalidSpiderDataException;
 import com.WMS_spiders_wholesale_system.exception.SpiderNotFoundException;
 import com.WMS_spiders_wholesale_system.repository.SpiderRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+
 @Service
 public class SpiderService {
 
     private final SpiderRepository spiderRepository;
+    private static final Logger logger = LoggerFactory.getLogger(SpiderService.class);
 
     @Autowired
     public SpiderService(SpiderRepository spiderRepository) {
@@ -20,18 +25,23 @@ public class SpiderService {
     }
 
     public Spider addSpider(Spider spider) {
+        validateSpider(spider);
+        logger.info("Added spider: " + spider.getId() + " " + spider.getSpeciesName() + " " + spider.getTypeName());
         return spiderRepository.save(spider);
     }
 
     public Spider updateSpider(Spider spider) {
-        if (!spiderRepository.existsById(spider.getId())) {
-            throw new SpiderNotFoundException("Spider with id " + spider.getId() + " not found");
-        }
+        Spider existingSpider = spiderRepository.findById(spider.getId())
+                .orElseThrow(() -> new SpiderNotFoundException("Spider with id " + spider.getId() + " not found"));
         return spiderRepository.save(spider);
     }
 
-    public void removeSpider(UUID id) {
+    public Spider getSpiderById(UUID id) {
+        return spiderRepository.findById(id)
+                .orElseThrow(() -> new SpiderNotFoundException("Spider with id " + id + " not found"));
+    }
 
+    public void removeSpider(UUID id) {
         if (!spiderRepository.existsById(id)) {
             throw new SpiderNotFoundException("Spider with ID " + id + " not found");
         }
@@ -42,5 +52,21 @@ public class SpiderService {
         return spiderRepository.findAll();
     }
 
+    private void validateSpider(Spider spider) {
+        if (spider == null) {
+            throw new InvalidSpiderDataException("Spider cannot be null");
+        }
+
+        if (spider.getSpeciesName() == null && spider.getTypeName() == null) {
+            throw new InvalidSpiderDataException("Spider species name or type name cannot be null");
+        }
+        if (spider.getSize() == null) {
+            throw new InvalidSpiderDataException("Spider size cannot be null");
+        }
+        if (spider.getQuantity() > 1) {
+            throw new InvalidSpiderDataException("Spider quantity cannot smaller than 0");
+        }
+        logger.info("Validating spider: " + spider.getId());
+    }
 
 }
